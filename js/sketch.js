cacf4a957f00ef63a4b9e0408c0ce160f2b05634
Derrick <arch.mccormick@gmail.com>
2021-02-03 17:49:09 -0600

    Updated

--- js/sketch.js
+++ js/sketch.js
@@ -4,44 +4,106 @@ function setup() {
   // put setup code here to run once
 
   // creating a canvas 500px by 500px
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1920, 960);
 
-  background(180, 60, 220);
+  //background color in R,G,B
+  background(0, 0, 0);
 
   //print(windowWidth);
-
   //change rectangle x,y coordinated to center of rect
   rectMode(CENTER);
+
 }
 
 function draw() {
 
   smooth();
 
-  //styles for the point
-  fill(120);
-  stroke(40, 200, 100);
-  strokeWeight(10);
-  //point is only x,y coordinates
-  point(mouseX, mouseY);
 
-  //styles for the ellipse
+  // Square at location (X, Y) with a side size of XX.
+  square(960, 480, 960);
   noStroke();
-  // put drawing code here to run in a loop
-  ellipse(300, 500, 150, 150);
+  fill(0, 0, 0);
 
-  //styles for the rect
-  stroke(60, 100, 80);
-  strokeWeight(20);
-  fill(50, 200, 120);
-  rect(800, 300, 200, 300);
+  // next sqaure
+  square(960, 480, 900);
+  noStroke();
+  fill(25, 25, 25);
 
-  // styles for the triangle
-  stroke(40, 200, 100);
-  strokeWeight(20);
-  fill(50, 30, 60)
-  // triangle needs to have 3 coordinates
-  triangle(1200, 50, 1300, 600, 950, 400);
+  // next sqaure
+  square(960, 480, 840);
+  noStroke();
+  fill(51, 51, 51);
 
-  print(mouseX);
+  // next sqaure
+  square(960, 480, 780);
+  noStroke();
+  fill(76, 76, 76)
+
+  // next sqaure
+  square(960, 480, 720);
+  noStroke();
+  fill(102, 102, 102)
+
+  // next sqaure
+  square(960, 480, 660);
+  noStroke();
+  fill(127, 127, 127)
+
+  // next sqaure
+  square(960, 480, 600);
+  noStroke();
+  fill(140, 140, 140)
+
+  // next sqaure
+  square(960, 480, 540);
+  noStroke();
+  fill(153, 153, 153)
+
+  // next sqaure
+  square(960, 480, 480);
+  noStroke();
+  fill(165.5, 165.5, 165.5)
+
+  // next sqaure
+  square(960, 480, 420);
+  noStroke();
+  fill(178, 178, 178);
+
+  // next sqaure
+  square(960, 480, 360);
+  noStroke();
+  fill(191, 191, 191);
+
+  // next sqaure
+  square(960, 480, 300);
+  noStroke();
+  fill(204, 204, 204);
+
+  // next sqaure
+  square(960, 480, 240);
+  noStroke();
+  fill(216.5, 216.5, 216.5);
+
+  // next sqaure
+  square(960, 480, 180);
+  noStroke();
+  fill(229, 229, 229);
+
+  // next sqaure
+  square(960, 480, 120);
+  noStroke();
+  fill(242, 242, 242);
+
+  // next sqaure
+  square(960, 480, 60);
+  noStroke();
+  fill(225, 255, 255);
+
+
+  // outermost square
+
+  // innermost square
+
+
 }
